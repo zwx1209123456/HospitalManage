30ccf88c84dbef6328598527ffdba6c0fdb6e5c3
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2019-01-05 10:13:22 +0800

    d1

--- HospitalManage/HospitalManage/Controllers/TierController.cs
+++ HospitalManage/HospitalManage/Controllers/TierController.cs
@@ -6,12 +6,48 @@ using System.Web.Mvc;
 
 namespace HospitalManage.Controllers
 {
+    using Unity.Attributes;
+    using IServices;
+    using Models;
+
     public class TierController : Controller
     {
+        [Dependency]
+        public ITierService tierService { get; set; }
         // GET: Tier
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public List<Tier> AddTier(Tier tier)
+        {
+            tierService.AddTier(tier);
+            var list = tierService.SelectTier();
+            return list;
+        }
+
+        [HttpPost]
+        public List<Tier> DelTier(int id)
+        {
+            tierService.DelTier(id);
+            var list = tierService.SelectTier();
+            return list;
+        }
+
+        [HttpGet]
+        public List<Tier> SelectTier()
+        {
+            var list = tierService.SelectTier();
+            return list;
+        }
+
+        [HttpPost]
+        public List<Tier> UpdateTier(Tier tier)
+        {
+            tierService.UpdateTier(tier);
+            var list = tierService.SelectTier();
+            return list;
         }
     }
 }
