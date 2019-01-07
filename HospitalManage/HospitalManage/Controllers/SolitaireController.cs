95510dd7f2a18a763d561822885fa9e04968624c
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2019-01-07 21:54:24 +0800

    dl7

---
+++ HospitalManage/HospitalManage/Controllers/SolitaireController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManage.Controllers
+{
+    public class SolitaireController : Controller
+    {
+        // GET: Solitaire
+        public ActionResult Index()
+        {
+            List<string []> w =new List<string[]> { new string[] { "1", "lkjlj", "khll", "ghg" }, new string[] { "jkh", "lkjlj", "khll", "ugjjhjg" }, new string[] { "jkh", "lkjlj", "khll", "ugjjg" } };
+        
+            ViewBag.y = w;
+            return View();
+        }
+
+        public ActionResult AddChains()
+        {
+            return View();
+        }
+
+        public ActionResult UodateChains()
+        {
+            return View();
+        }
+
+        public ActionResult AddMember()
+        {
+            return View();
+        }
+    }
+}
